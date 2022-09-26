9a62d8927e0f4194d02010b119b57e5a04aa54e6
Tomasz Siwek <tomasz.siwek0@gmail.com>
2022-09-26 15:19:56 +0200

    removed additional import

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 import { MockedProvider } from "@apollo/client/testing";
 import { FLIGHT_LIST_QUERY } from "./views/FlightList/FlightListPageGraphQL";
